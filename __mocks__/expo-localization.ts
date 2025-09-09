d0528fb86db1201ee0318eb49d4a905e18c4effd
relmersson <rasmus.elmersson@qred.com>
2025-09-09 07:11:49 +0200

    build: mock new localization

--- __mocks__/expo-localization.ts
+++ __mocks__/expo-localization.ts
@@ -2,3 +2,28 @@ export const locale = 'en-US';
 export const locales = ['en-US'];
 export const timezone = 'UTC';
 export const isRTL = false;
+
+// Add the new getLocales function for tests
+export const getLocales = () => [
+  {
+    languageTag: 'en-US',
+    languageCode: 'en',
+    textDirection: 'ltr',
+    digitGroupingSeparator: ',',
+    decimalSeparator: '.',
+    measurementSystem: 'us',
+    currencyCode: 'USD',
+    currencySymbol: '$',
+    regionCode: 'US',
+    temperatureUnit: 'fahrenheit',
+  },
+];
+
+export const getCalendars = () => [
+  {
+    calendar: 'gregory',
+    timeZone: 'UTC',
+    uses24hourClock: false,
+    firstWeekday: 1,
+  },
+];
