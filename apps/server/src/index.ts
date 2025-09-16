3c654440e93830e40d978fd39f949e5c335f489d
relmersson <rasmus.elmersson@qred.com>
2025-09-16 21:27:57 +0200

    feat: restart

---
+++ apps/server/src/index.ts
@@ -0,0 +1,25 @@
+import "dotenv/config";
+import cors from "cors";
+import express from "express";
+
+const app = express();
+
+app.use(
+	cors({
+		origin: process.env.CORS_ORIGIN || "",
+		methods: ["GET", "POST", "OPTIONS"],
+	}),
+);
+
+app.use(express.json());
+
+app.get("/", (_req, res) => {
+	setTimeout(() => {
+		res.status(200).send("OK");
+	}, 2000);
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+	console.log(`Server is running on port ${port}`);
+});
