58a4a079ebd131ad740ee6066d094a1218d74758
relmersson <rasmus.elmersson@qred.com>
2025-09-29 20:01:56 +0200

    refactor: project endpoint

--- apps/web/src/components/projects/index.ts
+++
@@ -1,4 +0,0 @@
-export { ApiMethodsInfo } from "./api-methods-info";
-export { CreateProjectCard } from "./create-project-card";
-export { ProjectForm } from "./project-form";
-export { ProjectsList } from "./projects-list";
